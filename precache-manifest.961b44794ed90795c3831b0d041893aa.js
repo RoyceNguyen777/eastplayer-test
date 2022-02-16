1980d75ce0d93abb673d89210bb6bc2af4700883
thunguyen <nguyenthahthu2014@gmail.com>
2022-02-16 12:59:36 +0700

    Updates

---
+++ precache-manifest.961b44794ed90795c3831b0d041893aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7ba3fce6883a049d4abedcfabfd68f",
+    "url": "/eastplayer-test/index.html"
+  },
+  {
+    "revision": "21c0a76777a2b8ca9ab7",
+    "url": "/eastplayer-test/static/css/main.b671eeb2.chunk.css"
+  },
+  {
+    "revision": "31bc6ffe02b5fea55e6a",
+    "url": "/eastplayer-test/static/js/2.3ad6b5c5.chunk.js"
+  },
+  {
+    "revision": "21c0a76777a2b8ca9ab7",
+    "url": "/eastplayer-test/static/js/main.a495a906.chunk.js"
+  },
+  {
+    "revision": "92be94e04db67747c548",
+    "url": "/eastplayer-test/static/js/runtime-main.01f38797.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RoyceNguyen777/eastplayer-test/precache-manifest.ff85335dc699ee2f87de8be94dcc6e22.js"
+  "/eastplayer-test/precache-manifest.961b44794ed90795c3831b0d041893aa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoyceNguyen777/eastplayer-test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eastplayer-test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
